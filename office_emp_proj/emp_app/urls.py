597fbcaad9027a1f2966068729176621716ad9a9
HenaDhawa <henadhawa@gmail.com>
2024-03-01 14:09:00 +0530

    first commit

---
+++ office_emp_proj/emp_app/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('emplogin/', views.emplogin, name='emplogin'),
+    path('home/', views.home, name='home'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+]
